9b8b85b9e902070250e8ab6226f179e15ae5fd65
Diego.W <Di3LuX_11@hotmail.es>
2023-01-02 15:59:35 +0100

    Create basic body of my react front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import contentSlice from "../slices/contentSlice";
+import userSlice from "../slices/userSlice";
+
+export default configureStore({
+  reducer: {
+    user: userSlice,
+    content: contentSlice,
+  },
+});
